699e35494e0b4147ae356c4a276391ef8b937fd9
Matias P. Brutti <matiasbrutti@gmail.com>
2016-01-15 08:48:21 -0800

    Adding support for AWS
    The new module will setup an instance in AWS us-east-1 and create an
    AMI based of it. Which will replicated to us-west-1 as well. Default
    user is ubuntu and keys will be created for you.

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 VM_USERNAME=""
 VM_PASSWORD=""
 VM_TYPE=""
+AWS_ACESSS_KEY=""
+AWS_SECRET_KEY=""
 
 function run_packer_vmware {
   packer build --only=vmware-iso -var "username=$VM_USERNAME" -var "password=$VM_PASSWORD" template.json
@@ -13,8 +15,7 @@ function run_packer_vbox {
 }
 
 function run_packer_aws {
-  echo "Not implemented yet"
-  # packer build --only=aws -var "username=$VM_USERNAME" -var "password=$VM_PASSWORD" ubuntu/template.json
+	 packer build --only=amazon-ebs -var "username=$VM_USERNAME" -var "password=$VM_PASSWORD" template.json
 }
 
 function generate_pressed {
@@ -31,18 +32,22 @@ function help {
   echo "optionals:"
   echo "  -u <username>\t\t Username of virtual machine."
   echo "  -p <password>\t\t Password of Virtual machine. Not recommended leaks creds."
+  echo "  -a <aws_access_key>\t\t AWS Accesss Key."
+	echo "  -s <aws_secret_key>\t\t AWS Secret Key."
 }
 
 # Main Application
-[[ ! $1 ]] && { help; exit 0; }
+[[ ! $2 ]] && { help; exit 0; }
 
 #Note that 'b' switch is broken
-while getopts "t:u:p:h" OPT; do
+while getopts "t:u:p:a:s:h" OPT; do
   case $OPT in
   h) help; exit 1;;
   t) VM_TYPE="$OPTARG";;
   u) VM_USERNAME="$OPTARG";;
   p) VM_PASSWORD="$OPTARG";;
+	a) AWS_ACESSS_KEY="$OPTARG";;
+	s) AWS_SECRET_KEY="$OPTARG";;
   *) echo "Unknown Command"; exit 1;;
   esac
 done
@@ -52,11 +57,11 @@ if [ -z $VM_TYPE ]; then
   exit 1;
 fi
 
-if [[ "${VM_USER}" = "" ]]; then
+if [[ "${VM_USERNAME}" = "" && "${VM_TYPE}" != "aws" ]]; then
   read -p  "Enter username: " VM_USERNAME
 fi
 
-if [[ "${VM_PASSWORD}" = "" ]]; then
+if [[ "${VM_PASSWORD}" = "" && "${VM_TYPE}" != "aws" ]]; then
   read -s -p "Enter Password: " VM_PASSWORD
 fi
 
@@ -71,8 +76,10 @@ case "$VM_TYPE" in
     run_packer_vbox
   ;;
   aws)
-    echo "[!] - currently not implemented"
-    exit 1
+		export AWS_ACCESS_KEY_ID=$AWS_ACESSS_KEY
+	  export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY
+		env 
+		run_packer_aws
   ;;
   *)
     echo "[!] - ERROR: You are querying an un-supported platform!"
